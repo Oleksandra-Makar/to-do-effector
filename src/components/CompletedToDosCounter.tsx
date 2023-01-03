954f7aac8c705a4dbe43c40e40c04b71a30037a6
Oleksandra Makar <oleksandra.makar@dataart.com>
2023-01-03 10:47:40 +0100

    added completed todos counter

--- src/components/CompletedToDosCounter.tsx
+++ src/components/CompletedToDosCounter.tsx
@@ -1,9 +1,14 @@
 import { Typography } from '@mui/material'
+import $store from '../store/todoStore'
+import { useStore } from 'effector-react'
 
 const CompletedToDosCounter = () => {
+    const { todos } = useStore($store)
+    const completedTodos = todos.filter((todo) => todo.completed === true).length
+
     return (
         <Typography style={{ textAlign: 'center' }} variant="h4">
-            Total completed to-dos: {2}
+            Total completed to-dos: {completedTodos}
         </Typography>
     )
 }
